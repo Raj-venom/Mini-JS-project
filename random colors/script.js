0ae79dce58117b72c7e57c5a370ae94a64824429
Raj Singh <rajsingh.rsr9824@gmail.com>
2023-09-12 18:28:47 +0545

    added copy color code options
    a copy option is added so that user can copy the hex color code value

--- random colors/script.js
+++ random colors/script.js
@@ -1,32 +1,40 @@
-
 const text = document.querySelector(".color-code");
 const btn = document.querySelector(".btn");
+const img = document.querySelector(".img");
 
+// Declared colorcode outside the function to use colorCode out side the function also
+let colorcode = "";
 
-function changeColor(){
+function changeColor() {
 
     let randomNumber = Math.floor(Math.random() * 16777215);
-    let colorcode = "#" + randomNumber.toString(16);
+    colorcode = "#" + randomNumber.toString(16);
+   
 
-    // to change background color
-    document.body.style.backgroundColor = colorcode;
-    // to change color code text
-    text.innerHTML = colorcode;
+  // to change background color
+  document.body.style.backgroundColor = colorcode;
+  // to change color code text
+  text.innerHTML = colorcode;
 
-    // to change button color according to background
-    btn.style.backgroundColor= colorcode;
+  // to change button color according to background
+  btn.style.backgroundColor = colorcode;
 
-    
 
-    console.log( randomNumber , colorcode)
+  console.log(randomNumber, colorcode);
+
+
 }
 
+// click event to change color
+btn.addEventListener("click", changeColor);
+text.addEventListener("click", changeColor);
 
-btn.addEventListener("click", changeColor)
-text.addEventListener("click", changeColor)
+// to copy text
+img.addEventListener("click", copy);
 
-
-
+function copy() {
+  navigator.clipboard.writeText(colorcode);
+}
 
 // initial call or fuction call after realoding page
-changeColor()
+changeColor();
