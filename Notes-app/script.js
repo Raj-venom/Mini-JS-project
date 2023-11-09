063ddfe5ae50e881368176582782b108fe78a840
Raj-venom <rajsingh.rsr9824@gmail.com>
2023-11-09 11:26:48 +0545

    Notes app using js

---
+++ Notes-app/script.js
@@ -0,0 +1,65 @@
+"use strict"
+
+const addBtn = document.querySelector("#addBtn");
+const main = document.querySelector("#main");
+
+const saveNotes = () => {
+    const notes = document.querySelectorAll('.note textarea');
+    let data = []
+
+    notes.forEach((note) => {
+
+        data.push(note.value)
+    })
+
+    localStorage.setItem('notes', JSON.stringify(data))
+}
+
+const loadNotes = () => {
+    let data = localStorage.getItem("notes");
+    data = JSON.parse(data)
+
+    console.log(data.length)
+
+    if (data.length == 0) {
+
+        addNotes()
+    }
+    else {
+
+        data.forEach((text) => {
+            addNotes(text)
+        })
+    }
+
+}
+
+
+const addNotes = (text = '') => {
+    const note = document.createElement('div');
+    note.classList.add('note')
+    note.innerHTML = `
+     <div class="tool">
+     <i class="fas fa-save"></i>
+
+    <i class="fas fa-trash"></i>
+    </div>
+    <textarea spellcheck="false" >${text}</textarea>
+     `
+    console.log(text)
+
+    note.querySelector('.fa-trash').addEventListener('click', function () {
+        note.remove()
+        saveNotes()
+    })
+
+    note.querySelector('textarea').addEventListener('focusout', function () {
+        saveNotes()
+    })
+
+    main.appendChild(note);
+    saveNotes()
+
+}
+
+loadNotes()
